7605c1e8de4688c73ff8907ae42872afb6c8f974
MagcalasGit <162887234+MagcalasGit@users.noreply.github.com>
2024-03-24 19:17:10 +0800

    Add files via upload

---
+++ loginLastoutput/loginLastoutput/AccDisbaled.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace loginLastoutput
+{
+    partial class AccDisbaled
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AccDisbaled));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            BtnOkay = new Guna.UI2.WinForms.Guna2Button();
+            pictureBox1 = new PictureBox();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // BtnOkay
+            // 
+            BtnOkay.BorderRadius = 15;
+            BtnOkay.BorderThickness = 1;
+            BtnOkay.CustomizableEdges = customizableEdges1;
+            BtnOkay.DisabledState.BorderColor = Color.DarkGray;
+            BtnOkay.DisabledState.CustomBorderColor = Color.DarkGray;
+            BtnOkay.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BtnOkay.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BtnOkay.FillColor = Color.DarkSlateGray;
+            BtnOkay.Font = new Font("Segoe UI Emoji", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BtnOkay.ForeColor = Color.White;
+            BtnOkay.Location = new Point(12, 224);
+            BtnOkay.Name = "BtnOkay";
+            BtnOkay.PressedColor = Color.Teal;
+            BtnOkay.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            BtnOkay.Size = new Size(180, 45);
+            BtnOkay.TabIndex = 0;
+            BtnOkay.Text = "Okay";
+            BtnOkay.Click += BtnOkay_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(274, 114);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(203, 166);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.DarkSlateGray;
+            guna2HtmlLabel1.Font = new Font("Segoe UI Emoji", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2HtmlLabel1.ForeColor = Color.Transparent;
+            guna2HtmlLabel1.Location = new Point(12, 12);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(205, 34);
+            guna2HtmlLabel1.TabIndex = 2;
+            guna2HtmlLabel1.Text = "Account Disabled";
+            // 
+            // guna2TextBox1
+            // 
+            guna2TextBox1.CustomizableEdges = customizableEdges3;
+            guna2TextBox1.DefaultText = "";
+            guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Font = new Font("Segoe UI", 9F);
+            guna2TextBox1.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Location = new Point(12, 52);
+            guna2TextBox1.Name = "guna2TextBox1";
+            guna2TextBox1.PasswordChar = '\0';
+            guna2TextBox1.PlaceholderText = "";
+            guna2TextBox1.ReadOnly = true;
+            guna2TextBox1.SelectedText = "";
+            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2TextBox1.Size = new Size(242, 153);
+            guna2TextBox1.TabIndex = 3;
+            // 
+            // AccDisbaled
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightCyan;
+            ClientSize = new Size(478, 281);
+            Controls.Add(guna2TextBox1);
+            Controls.Add(guna2HtmlLabel1);
+            Controls.Add(pictureBox1);
+            Controls.Add(BtnOkay);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "AccDisbaled";
+            Text = "AccDisbaled";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button BtnOkay;
+        private PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+    }
+}
